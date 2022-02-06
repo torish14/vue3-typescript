506c966fe97038f0f1ddc7b9dc14f787dab8f71e
torish14 <nanamin.104@icloud.com>
2022-02-06 17:45:26 +0900

    🗑 removal: 不要なコードを削除

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
@@ -16,12 +15,4 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
